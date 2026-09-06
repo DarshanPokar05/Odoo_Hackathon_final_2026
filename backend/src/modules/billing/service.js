a4502af2806e625944ffb4507ffee4a8651b9d92
N-Bhavsar <147849376+N-Bhavsar@users.noreply.github.com>
2026-09-06 10:56:03 +0530

    Delete backend directory

--- backend/src/modules/billing/service.js
+++
@@ -1,8 +0,0 @@
-﻿'use strict';
-const prisma = require('../../config/prisma');
-// Monthly billing job calls into this service. Full implementation in Phase 5.
-exports.list   = async () => prisma.invoice.findMany({ where: { type: 'RECURRING' } });
-exports.getOne = async (id) => prisma.invoice.findUniqueOrThrow({ where: { id } });
-exports.create = async (body) => prisma.invoice.create({ data: body });
-exports.update = async (id, body) => prisma.invoice.update({ where: { id }, data: body });
-exports.remove = async (id) => prisma.invoice.delete({ where: { id } });
--- backend/src/modules/dealHealth/controller.js
+++
@@ -1,9 +0,0 @@
-﻿'use strict';
-
-const svc         = require('./service');
-const { success } = require('../../utils/response');
-
-exports.list     = async (req, res, next) => { try { res.json(success(await svc.list(req.query, req.user))); } catch (e) { next(e); } };
-exports.getOne   = async (req, res, next) => { try { res.json(success(await svc.getOne(req.params.id, req.user))); } catch (e) { next(e); } };
-exports.resolve  = async (req, res, next) => { try { res.json(success(await svc.resolve(req.params.id, req.user))); } catch (e) { next(e); } };
-exports.escalate = async (req, res, next) => { try { res.json(success(await svc.escalate(req.params.id, req.body, req.user))); } catch (e) { next(e); } };
